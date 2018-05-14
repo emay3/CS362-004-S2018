9995abfc5a94e384f08823ed2262998cfca79418
emay3 <mayel@oregonstate.edu>
2018-05-13 22:21:43 -0700

    Commit initial randomtesters

---
+++ projects/mayel/dominion/randomtestcard2.c
@@ -0,0 +1,20 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int main(){
+
+
+
+
+
+return 0;
+
+
+}
