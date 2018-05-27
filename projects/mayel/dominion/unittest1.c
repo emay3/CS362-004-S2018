5021cab46c206222153b1b90976c5f5a6608391f
emay3 <mayel@oregonstate.edu>
2018-05-27 16:48:26 -0700

    update again

---
+++ projects/mayel/dominion/unittest1.c
@@ -0,0 +1,30 @@
+/*************************************************************
+**Elizabeth May (mayel)
+**4-29-2018
+**cs362 - Assignment 3 - unittest1.c
+** Testing : 
+*****************************************/
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+
+#define IS_OVER 1
+#define NOT_OVER 0 
+ 
+//global variables 
+
+int passCount = 0;
+int failCount = 0;
+int total = 0; 
+
+int main(){
+
+//
+
+return 0;
+}
